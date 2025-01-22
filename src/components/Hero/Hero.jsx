4a3a630114e135982e8279982c174483235eae92
Jonathan Luembe <jonathanluembe@yahoo.com>
2025-01-22 14:03:59 +0100

    Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,32 +1,42 @@
-import React from "react";
-import Image1 from "../../assets/hero/Image-Homme-1.jpg";
-import Image2 from "../../assets/hero/Image-Homme-2.jpg";
-import Image3 from "../../assets/hero/Image-Homme-3.jpg";
-import black from "../../assets/hero/black-panther.png";
+import Image1 from "../../assets/hero/black-panther.png";
+import Image2 from "../../assets/hero/Damson_idris.png";
+import Image3 from "../../assets/hero/Jonas.png";
 import Slider from "react-slick";
 
 const ImageList = [
   {
     id: 1,
     img: Image1,
-    title: "Test 1",
-    description: "Description 1",
+    title: "Tenue traditionnelle",
+    description: " Alliez classe et confort avec nos costumes sur mesure.",
   },
   {
     id: 2,
     img: Image2,
-    title: "Test 2",
-    description: "Description 2",
+    title: "Tapis rouge",
+    description: "Adoptez un style moderne avec nos costumes tendance.",
   },
   {
     id: 3,
     img: Image3,
-    title: "Test 3",
-    description: "Description 3",
+    title: "Mariage",
+    description: "Découvrez l'élégance intemporelle du costume noir.",
   },
 ];
 
 const Hero = () => {
+  var settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
   return (
     <div
       className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex
@@ -40,45 +50,40 @@ const Hero = () => {
       ></div>
       {/* Hero section */}
       <div className="container pb-8 sm:pb-0">
-        <Slider>
-
-        <div>
-          <div className="grid grid-cols-1 sm:grid-cols-2">
-            {/* text content section */}
-            <div 
-            className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left
-            order-2 sm:order-1 relative z-10"
-            >
-              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
-                Test{" "}
-              </h1>
-              <p className="text-sm">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Numquam, nemo!
-              </p>
-              <div>
-                <button
-                  className="bg-gradient-to-r from-primary to-secondary 
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div key={data.id}>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left
+                    order-2 sm:order-1 relative z-10">
+                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                    {data.title}
+                  </h1>
+                  <p className="text-sm">{data.description}</p>
+                  <div>
+                    <button
+                      className="bg-gradient-to-r from-primary to-secondary 
                         hover:scale-105 duration-200 text-white py-2 px-4 rounded-full"
-                >
-                  Order Now
-                </button>
+                    >
+                      Order Now
+                    </button>
+                  </div>
+                </div>
+                {/* image section */}
+                <div className="order-1 sm:order-2">
+                  <div className="relative z-10">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px]
+                                object-contain mx-auto"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            {/* image section */}
-            <div 
-            className="order-1 sm:order-2"
-            >
-                <div 
-                className="relative z-10">
-                    <img src={black} alt="" 
-                    className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125
-                     object-contain mx-auto"
-                    />
-                </div>
-            </div>
-          </div>
-        </div>
+          ))}
         </Slider>
       </div>
     </div>
